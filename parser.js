109092638040c2c4bc1a5debb370f0f4d2476724
Rudolf Ádám <sari.vogel.94@gmail.com>
2018-12-30 22:47:09 +0100

    Loop through the input letter-by-letter

--- parser.js
+++ parser.js
@@ -2,5 +2,7 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 fs.readFile('code.txt', function(err, data) {
-    process.stdout.write(data);
+    for (var i = 0; i < data.length; i++) {
+        process.stdout.write(String(data).charAt(i));
+    }
 });
