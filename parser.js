e132d78a8501770197ac1d6aecb0588bc6aa8b74
Rudolf Ádám <sari.vogel.94@gmail.com>
2018-12-31 14:11:42 +0100

    syntax highlight

--- parser.js
+++ parser.js
@@ -2,7 +2,20 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 fs.readFile('code.txt', function(err, data) {
+
+    var isColorRose = false;
+
     for (var i = 0; i < data.length; i++) {
-        process.stdout.write(String(data).charAt(i));
+        if (String(data).charAt(i) == "<") {
+            isColorRose = true;
+        }
+        if (isColorRose) {
+            process.stdout.write(chalk.magenta(String(data).charAt(i)));
+        } else {
+            process.stdout.write(chalk.blue(String(data).charAt(i)));
+        }
+        if (String(data).charAt(i) == ">") {
+            isColorRose = false;
+        }
     }
 });
